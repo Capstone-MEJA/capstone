60adbedc000e1980ac8e1499e6ba12bd6a58aea3
Erica Young <eyoung135@gmail.com>
2023-03-15 17:09:04 -0400

    figured out why our connections are working and named/logo project

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const db = mongoose.connect('mongodb://127.0.0.1:27017/maps')
+const db = mongoose.connect('mongodb://127.0.0.1:27017/memorymarker')
 
 module.exports = db
--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-const db = require('./db/db')
-const PORT = process.env.PORT || 8080
+// const db = require('./db/db')
+const PORT = process.env.PORT || 3000
 const app = require('./app')
 const seed = require('../script/seed')
+const mongoose = require('mongoose')
+
+// const db = mongoose.connect('mongodb://127.0.0.1:27017/maps')
 
 const init = async () => {
 try {
 if (process.env.SEED === 'true') {
 await seed()
 } else {
-await db.sync()
+    // await mongoose.connect('mongodb://127.0.0.1:27017/maps')
+// await db.sync()
 }
 // start listening (and create a 'server' object representing our server)
 app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
