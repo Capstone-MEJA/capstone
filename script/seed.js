f70c7f94f60be6a46baf869b504798631f1cd093
Erica Young <eyoung135@gmail.com>
2023-03-15 17:21:35 -0400

    added console logs and comments

--- script/seed.js
+++ script/seed.js
@@ -3,27 +3,33 @@ const Post = require('../server/models/Post')
 const db = require('../server/db/db')
 const mongoose = require('mongoose')
 
+// this is the seed function
 async function seed() {
-    //dropping the db
-    //if this does not work we could google more stuff later
-    //--------------THIS IS THE PROBLEM-------------
+    console.log('deleting table content!')
     await User.deleteMany({})
     await Post.deleteMany({})
-    // await db.Post.drop()
-    // creating 
+    console.log('table contents have been deleted!')
+    
+    console.log('seeding users')
+    
     const users = await Promise.all([
         User.create({ username: 'jessie', password: '123' }),
         User.create({ username: 'mandy', password: '123' }),
         User.create({ username: 'erica', password: '123' }),
         User.create({ username: 'alicia', password: '123' }),
-        ])
+      ])
+    console.log('user seeded')
+
+    console.log('seeding posts')
     const posts = await Promise.all([
         Post.create({ title: 'Hi, its me', description: 'im the problem, its me',
         tags: ['#food', '#life', '#song'], latitude: 234, longitude: 345.963 
         })
     ])
+    console.log('posts seeded')
     }
 
+// this is the function that runs the seed
     async function runSeed() {
         console.log('seeding...')
         try {
